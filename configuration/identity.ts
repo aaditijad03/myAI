898901bfa3daeafa26619215f942f216f8f6995b
Aaditi Jadhav <107875217+aaditijad03@users.noreply.github.com>
2025-03-03 12:12:56 -0500

    Update AI name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Aaditi Jadhav`;
 export const OWNER_DESCRIPTION: string = `Student at Chapel Hill`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Finance Buddy`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
