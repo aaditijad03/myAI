35cae43f655554e7365d247dc33f5e10546e5af3
Aaditi Jadhav <107875217+aaditijad03@users.noreply.github.com>
2025-03-03 12:18:11 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Chat with Cryptobot`;
+export const CHAT_HEADER: string = `Chat with Finance Buddy`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Aaditi Jadhav`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
-export const PAGE_TITLE: string = `Learn Basics of Cryptocurrency`;
+export const PAGE_TITLE: string = `Learn Basics of Financial Planning`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
